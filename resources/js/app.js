29b97e7cad201d1a1b607061dd7df85f9a7f9dbd
Fadlan Hidayat <mfh.fadlanhidayat.com>
2025-06-29 02:34:34 +0700

    Fix: duplicate hideNotification declaration

--- resources/js/app.js
+++ resources/js/app.js
@@ -242,15 +242,15 @@ window.KoperasiApp = {
   // ... fungsi lain ...
 };
 
-function hideNotification(notification) {
-  notification.style.opacity = "0"
-  notification.style.transform = "translateX(100%)"
-  setTimeout(() => {
-    if (notification.parentNode) {
-      notification.parentNode.removeChild(notification)
-    }
-  }, 300)
-}
+// function hideNotification(notification) {
+//   notification.style.opacity = "0"
+//   notification.style.transform = "translateX(100%)"
+//   setTimeout(() => {
+//     if (notification.parentNode) {
+//       notification.parentNode.removeChild(notification)
+//     }
+//   }, 300)
+// }
 
 // Utility functions
 function formatCurrency(amount) {
